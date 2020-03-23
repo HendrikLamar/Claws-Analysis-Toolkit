5c2b295d3fe1d894b95a313a7e6ef513b215b2be
Hendrik Windel <15370203+HendrikLamar@users.noreply.github.com>
2020-03-23 19:58:47 +0100

    Cleanup

--- cat/phase2/_global.py
+++ cat/phase2/_global.py
@@ -1,6 +1,6 @@
 picos = ['Top_Forward', 'Top_Backward', 'Bottom_Forward', 'Bottom_Backward']
-#picos = ['FWD_Z0', 'FWD_Z1', 'FWD_Z2', 'FWD_Z3', 'BWD_Z0', 'BWD_Z1', 'BWD_Z2', 'BWD_Z3']
 channels = ['A', 'B', 'C','D']
+
 items_channel = {'1pe' : '1peNormalized',
         '1peVStotal' : '1peVStotal', 'mips' : 'mipAbsolute', 'reco' : 'wfReco', 'cdf' : 'cdf'}
 items_pico = {'run' : 'beast_runNumber', 'subRun' : 'beast_subRunNumber', 'ts' : 'timestamp',
